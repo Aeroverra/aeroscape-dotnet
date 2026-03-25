5d9455cf79dff5d53e8a764cc17d19c0682e11a5
Zoe <zoe@aeroverra.com>
2026-03-25 08:23:30 -0400

    feat: translated NPCAttack, ItemOperate, ObjectOption1, and ActionButtons packets to C# handlers

--- AeroScape.Server.Core/obj/Debug/net10.0/AeroScape.Server.Core.AssemblyInfo.cs
+++ AeroScape.Server.Core/obj/Debug/net10.0/AeroScape.Server.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AeroScape.Server.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e79dc98635e93f9b8a20f3a08a6be65395884ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+629b36625cdc14fa8729c43e8cdc464667bb5b4b")]
 [assembly: System.Reflection.AssemblyProductAttribute("AeroScape.Server.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AeroScape.Server.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
